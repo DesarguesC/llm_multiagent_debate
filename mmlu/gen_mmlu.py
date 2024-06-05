dfcda06c9388e75b8978ac13a52d753f05a6c430
DesarguesC <425593160@qq.com>
2024-06-05 16:47:29 +0800

    gen_mmlu remain

--- mmlu/gen_mmlu.py
+++ mmlu/gen_mmlu.py
@@ -1,8 +1,13 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from glob import glob
 import pandas as pd
 import json
+from claude_util import *
 import time
 import random
+from tqdm import tqdm
 import openai
 
 def construct_message(agents, question, idx):
@@ -21,21 +26,23 @@ def construct_message(agents, question, idx):
     return {"role": "user", "content": prefix_string}
 
 
-def construct_assistant_message(completion):
-    content = completion["choices"][0]["message"]["content"]
-    return {"role": "assistant", "content": content}
+def construct_assistant_message(completion, is_claude=False):
+    return {"role": "assistant", "content": completion.content[-1].text} if is_claude else \
+        {"role": "assistant", "content": completion["choices"][0]["message"]["content"]}
 
 
-def generate_answer(answer_context):
-    try:
-        completion = openai.ChatCompletion.create(
-                  model="gpt-3.5-turbo-0301",
-                  messages=answer_context,
-                  n=1)
-    except:
-        print("retrying due to an error......")
-        time.sleep(20)
-        return generate_answer(answer_context)
+def generate_answer(answer_context, claude_agent=None):
+    while True:
+        try:
+            completion = openai.ChatCompletion.create(
+                      model="gpt-3.5-turbo-0301",
+                      messages=answer_context,
+                      n=1) if claude_agent is None else \
+                        claude_agent.context_ask(answer_context)
+        except Exception as err:
+            print(err)
+            time.sleep(20)
+            continue
 
     return completion
 
@@ -54,17 +61,26 @@ def parse_question_answer(df, ix):
     return question, answer
 
 if __name__ == "__main__":
+    """
+        run file in main directory: 
+            python ./mmlu/gen_mmlu.py
+    """
+    # init Claude Agent
+    api_key = list(pd.read_csv('key.csv')['anthropic'])[0]
+    claude_agent = Claude(engine='claude-3-haiku-20240307', api_key=api_key, tokens=1000) # 300, 600 insuficient
+
     agents = 3
     rounds = 2
 
-    tasks = glob("/data/vision/billf/scratch/yilundu/llm_iterative_debate/mmlu/data/test/*.csv")
+    tasks = glob("./mmlu/data/test/*.csv")
 
     dfs = [pd.read_csv(task) for task in tasks]
 
     random.seed(0)
     response_dict = {}
-
-    for i in range(100):
+    tot_num = 5 # original: 100
+    start_time = time.time()
+    for i in tqdm(range(tot_num)):
         df = random.choice(dfs)
         ix = len(df)
         idx = random.randint(0, ix-1)
@@ -81,12 +97,14 @@ if __name__ == "__main__":
                     message = construct_message(agent_contexts_other, question, 2 * round - 1)
                     agent_context.append(message)
 
-                completion = generate_answer(agent_context)
+                completion = generate_answer(agent_context, claude_agent=claude_agent)
 
-                assistant_message = construct_assistant_message(completion)
+                assistant_message = construct_assistant_message(completion, is_claude=True)
                 agent_context.append(assistant_message)
                 print(completion)
 
         response_dict[question] = (agent_contexts, answer)
 
     json.dump(response_dict, open("mmlu_{}_{}.json".format(agents, rounds), "w"))
+    end_time = time.time()
+    print(f'request cost time = {end_time - start_time}') #
